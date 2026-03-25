93f91cdef0cebddbd9286e1b9d03ebba4472727a
Evan Baird <evanbaird@evans-mbp.lan>
2026-03-25 14:08:15 -0600

    Added some options.

--- src/typer_chucknorris/main.py
+++ src/typer_chucknorris/main.py
@@ -1,3 +1,4 @@
+from typing import Annotated
 import typer
 from rich import print
 import httpx
@@ -8,14 +9,44 @@ app = typer.Typer()
 
 
 @app.command()
-def chuck():
-    url = 'https://api.chucknorris.io/jokes/random'
+def chuck(
+    random: Annotated[bool, typer.Argument(help='Get random quote')] = True,
+    categories: Annotated[bool, typer.Option(help='Retrieve a list of available categories.')] = False,
+    category_select: Annotated[str, typer.Option(help='Select category and retrieve a quote from the entered category')] = "",
+):
+    """
+    Get a random bad a$$ Chuck Norris quote from the CLI!!!
+    """
 
-    resp = httpx.get(url)
+    if categories:
+        url = 'https://api.chucknorris.io/jokes/categories'
 
-    norris = dict(resp.json())
+        resp = httpx.get(url)
 
-    print(f"[blue bold]{norris['value']}[/ blue bold]")
+        print("\n[bold blue]The categories you can choose from are:[/]\n")
+
+        for _ in resp.json():
+            print(_)
+
+        print()
+
+    elif category_select:
+        url = 'https://api.chucknorris.io/jokes/random/'
+
+        resp = httpx.get(url, params={'category': category_select})
+
+        norris = dict(resp.json())
+
+        print(f"[green bold]{norris['value']}[/ green bold]")
+
+    elif random:
+        url = 'https://api.chucknorris.io/jokes/random'
+
+        resp = httpx.get(url)
+
+        norris = dict(resp.json())
+
+        print(f"[blue bold]{norris['value']}[/ blue bold]")
 
 
 if __name__ == "__main__":
