731528e9b4a3996d05320833f74ff1c7600e5b1e
Evan Baird <evanbaird@evans-mbp.lan>
2026-04-04 10:12:57 -0600

    created typer sub and cleaned.

---
+++ src/typer_chucknorris/chucknorris/copy_norris.py
@@ -0,0 +1,22 @@
+import httpx
+import pyperclip
+from rich import print
+
+
+def norris_quote():
+    """
+    Quick function https://api.chucknorris.io/jokes/random
+    of Chuck Norris funny quotes.
+    """
+
+    url = 'https://api.chucknorris.io/jokes/random'
+
+    resp = httpx.get(url)
+
+    norris = dict(resp.json())
+
+    print(f"[blue bold]{norris['value']}[/ blue bold]")
+    
+
+
+
--- src/typer_chucknorris/main.py
+++ src/typer_chucknorris/main.py
@@ -1,118 +1,18 @@
-import secrets
-from typing import Annotated
-import typer
-from rich import print
-import httpx
-import tomli
-from pathlib import Path
-# from chucknorris.categories import chuck_completions
+import sys
 
+import typer
+
+sys.path.append('/Users/evanbaird/Projects/Projects/typer_chucknorris/src/typer_chucknorris')
+from chucknorris import  norris
 
 
 app = typer.Typer(rich_markup_mode='rich', no_args_is_help=True)
-
-#TODO Put version snippet below in another file
-
-home = Path('../../pyproject.toml')
-
-with open(home, mode='br') as v:
-    version = tomli.load(v)
-
-chuck_list = [('animal', 'Chuck Norris and animals'), ('career', 'Chuck Norris and careers'), ('celebrity', 'Chuck Norris and other lesser celebrities'), ('dev', 'Chuck Norris and computer stuff'), ('explicit', 'Chuck Norris and explicit sh*t'), ('fashion', 'Chuck Norris and girl crap'), ('food', 'Chuck Norris and chow'), ('history', 'Chuck Norris making history'), ('money', 'Chuck Norris and money money'), ('movie', 'Chuck Norris movies and films'), ('music', 'Chuck Norris and tunes'), ('political', 'Chuck Norris and politics'), ('religion', 'Chuck Norris and being worshipped'), ('science', 'Chuck Norris and science'), ('sport', 'Chuck Norris and Male sports'), ('travel', 'Chuck Norris traveling to your mom')]
-
-
-def chuck_completions(incomplete: str):
-    for name, help_text in chuck_list:
-        if name.startswith(incomplete):
-            yield(name, help_text)
-
-def version_callback(value: bool):
-    """
-    Call back function for project version number
-    """
-    if value:
-        # print(f"Awesome Chuck Norris CLI Version: [bold green]{version['project']['version']}[/]")
-        print(f"Awesome Chuck Norris CLI Version: [bold green]{version['project']['version']}[/]")
-        raise typer.Exit()
-
-#TODO Everything above in another file.
-
-@app.command(epilog="[bold]In tribute to [link=https://www.imdb.com/title/tt0090927/]Chuck Norris[/link][/] :fist:!")
-def chuck(
-    random: Annotated[bool, typer.Argument(help='Get random quote')] = True,
-    categories: Annotated[bool, typer.Option('--categories', '-c', rich_help_panel='Options', help='Retrieve a list of available categories.')] = False,
-    category_select: Annotated[
-        str,
-        typer.Option(
-            '--category-select',
-            '-cs',
-            rich_help_panel='Options',
-            help='Select category and retrieve a quote from the entered category',
-            autocompletion=chuck_completions,
-        )
-    ] = "",
-    search: Annotated[str, typer.Option('--search', '-s', rich_help_panel='Options', help='Free text search to find relavent joke')] = "",
-    version: Annotated[bool | None, typer.Option("--version", "-v", rich_help_panel='Options', callback=version_callback, is_eager=True)] = None,
-):
-    """
-    Get a random bad a$$ Chuck Norris quote from the CLI!!!
-    """
-
-    if categories:
-
-        resp = httpx.get(url='https://api.chucknorris.io/jokes/categories')
-
-        print("\nThe categories you can choose from are:\n")
-
-        for _ in resp.json():
-            print(_)
-
-        print()
-
-    elif category_select:
-
-        resp = httpx.get(url='https://api.chucknorris.io/jokes/random', params={'category': category_select})
-
-        chuck  = dict(resp.json())
-
-        norris = chuck['value']
-
-        print(f"[bold blue]{norris}[/]\n")
-
-    elif search:
-
-        resp = httpx.get(url='https://api.chucknorris.io/jokes/search', params={'query': search})
-
-        chuck  = dict(resp.json())
-
-        norris = chuck['result'][secrets.randbelow(chuck['total'])]['value']
-
-        print(f"[bold green]{norris}[/]\n")
-
-    else:
-
-        resp = httpx.get(url='https://api.chucknorris.io/jokes/random')
-
-        norris = dict(resp.json())
-
-        print(f"[bold red]{norris['value']}[/]\n")
-
-
-#TODO: We'll see if we can add this back...
-#   @app.callback()
-#   def callback():
-#       """
-#       [bold yellow]Chuck Norris Quote dispenser![/bold yellow]
-
-#       Passing in the argument [code]chuck[/] to get a bad ass random Chuck Norris quote!
-
-#       Passing the option [code]--categories[/] [bold]|[/] [code]-c[/] will list Category quote option to choose from.
-
-#       Passing the option [code]--category-select[/] [bold]|[/] [code]-cs[/] and pass in the selected category you want the quote from.
-
-#       Passing the option [code]--search[/] [bold]|[/] [code]-s[/] and passing a search time will output a bad ass quote relative to the search term.
-#       """
+app.add_typer(norris.app)
 
 
 if __name__ == "__main__":
     app()
+
+
+
+
